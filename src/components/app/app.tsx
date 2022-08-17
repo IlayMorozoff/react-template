800ec9148eeedaa4106912e0dbb30682ade2f975
Ilay Morozov <morozoff1994@yandex.ru>
2022-08-17 22:47:21 +0300

    init

---
+++ src/components/app/app.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from "./app.module.scss";
+
+console.log(styles)
+
+export const App: React.FC = () => {
+  return (
+    <div className={styles.norm}>appdadadadasdaad</div>
+  )
+}
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './components/app/app';
+
+const container = document.getElementById('root');
+
+if (container === null) throw new Error('Root container missing in index.html')
+
+const root = ReactDOM.createRoot(container);
+
+root.render(<App />);
+
